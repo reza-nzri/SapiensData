4738cfebfcdaf9c231cb3651576b0d0903e7e4e0
janusz s <sliwinskij308@sus-am-bktm.de>
2024-11-06 12:49:18 +0100

    Do something with whitespace

--- SapiensDataAPI/SapiensDataAPI/Models/ApplicationUserModel.cs
+++ SapiensDataAPI/SapiensDataAPI/Models/ApplicationUserModel.cs
@@ -2,9 +2,9 @@
 
 namespace SapiensDataAPI.Models
 {
-    public class ApplicationUserModel : IdentityUser
-    {
-        public string FirstName { get; set; } = string.Empty;
+	public class ApplicationUserModel : IdentityUser
+	{
+		public string FirstName { get; set; } = string.Empty;
 		public string? MiddleName { get; set; }
 		public string LastName { get; set; } = string.Empty;
 		public string? Prefix { get; set; }
