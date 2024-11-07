a299cc20059346b64469b207d790c3e03d0043e3
janusz s <sliwinskij308@sus-am-bktm.de>
2024-11-07 10:23:17 +0100

    Remove useless things from db context

--- SapiensDataAPI/SapiensDataAPI/Data/DbContextCs/SapeinsDataContext.cs
+++ SapiensDataAPI/SapiensDataAPI/Data/DbContextCs/SapeinsDataContext.cs
@@ -68,17 +68,11 @@ public partial class SapeinsDataContext : IdentityDbContext<ApplicationUserModel
 
     public virtual DbSet<UnitType> UnitTypes { get; set; }
 
-    //public virtual DbSet<User> Users { get; set; }
-
     public virtual DbSet<UserAddress> UserAddresses { get; set; }
 
     public virtual DbSet<UserRelationship> UserRelationships { get; set; }
 
     public virtual DbSet<UserSession> UserSessions { get; set; }
-
-    /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=SapeinsData;Trusted_Connection=True;TrustServerCertificate=True; Encrypt=False");*/
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
