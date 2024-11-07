2228740cb18099524d364dc6c15e5b2f0e10fc76
janusz s <sliwinskij308@sus-am-bktm.de>
2024-11-07 13:46:11 +0100

    Add expense post api controller

--- SapiensDataAPI/SapiensDataAPI/Dtos/Expense/Request/ExpenseDto.cs
+++ SapiensDataAPI/SapiensDataAPI/Dtos/Expense/Request/ExpenseDto.cs
@@ -1,6 +1,29 @@
-﻿namespace SapiensDataAPI.Dtos.Expense.Request
+﻿using SapiensDataAPI.Models;
+
+namespace SapiensDataAPI.Dtos.Expense.Request
 {
 	public class ExpenseDto
 	{
+		public required string UserId { get; set; }
+
+		public string? SourceUserId { get; set; }
+
+		public string? SourceFirstName { get; set; }
+
+		public string? SourceLastName { get; set; }
+
+		public string? SourceCompanyName { get; set; }
+
+		public DateOnly ExpenseDate { get; set; }
+
+		public string? Description { get; set; }
+
+		public decimal Amount { get; set; }
+
+		public string? Currency { get; set; }
+
+		public string? PaymentStatus { get; set; }
+
+		public int FrequencyId { get; set; }
 	}
 }
