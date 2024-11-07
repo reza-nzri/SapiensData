51b369413aa5cdccd8ea6d6ca5f279cf96c6776c
janusz s <sliwinskij308@sus-am-bktm.de>
2024-11-07 13:31:31 +0100

    Add user_id, upload_date to reciept table for tagging in API

--- SapiensDataAPI/SapiensDataAPI/Migrations/20241107122734_Add user id and updload date to receipt table.Designer.cs
+++ SapiensDataAPI/SapiensDataAPI/Migrations/20241107122734_Add user id and updload date to receipt table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SapiensDataAPI.Data.DbContextCs;
 
@@ -11,9 +12,11 @@ using SapiensDataAPI.Data.DbContextCs;
 namespace SapiensDataAPI.Migrations
 {
     [DbContext(typeof(SapeinsDataContext))]
-    partial class SapeinsDataContextModelSnapshot : ModelSnapshot
+    [Migration("20241107122734_Add user id and updload date to receipt table")]
+    partial class Adduseridandupdloaddatetoreceipttable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
