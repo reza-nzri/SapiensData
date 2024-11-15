e3c276db847e831ee400bced4d5b1cf89d7042be
Reza Nazari <nazarir152@sus-am-bktm.de>
2024-11-15 03:53:11 +0100

    Update main.py

--- Analytics/src/main.py
+++ Analytics/src/main.py
@@ -5,9 +5,8 @@ import sys  # to handle command-line arguments
 from colorama import Fore, Style
 
 # Import necessary utility classes from the utils module
-from utils import CLI, ChatGPTClient, JsonUpdater, FolderChecker, JsonFormatter, \
-                   MdPropmtMaker, MetadataExtractor, OcrExtractor, PDFTextExtractor, ReceiptSender, \
-                   TempFileDeleter, TextAutoCorrector, TextCleaner
+from utils import CLI, ChatGPTClient, JsonUpdater, FolderChecker, JsonFormatter, JsonReceiptRemover, \
+                   MetadataExtractor, OcrExtractor, PDFTextExtractor, ReceiptSender, TextCleaner
 
 
 def main(source_image):
@@ -54,47 +53,57 @@ def main(source_image):
     ocr_extractor = OcrExtractor(username, receipt_path)
     ocr_extractor.extract_text()  # Perform OCR extraction
 
+    """
     # Step 8: Clean the extracted text (auto-correction and text formatting)
-    text_cleaner = TextAutoCorrector(username)
-    text_cleaner.load_and_process_text()  # Auto-correct the text
+    text_auto_corrector = TextAutoCorrector(username)
+    text_auto_corrector.load_and_process_text()  # Auto-correct the text
+    """
 
     # Step 9: Further process the cleaned text (for additional formatting)
     text_cleaner = TextCleaner(username)
     text_cleaner.load_and_process_text()  # Perform further text cleaning
-
+    
+    """
     # Step 10: Generate a prompt for the AI (GPT model)
     processor = MdPropmtMaker(username)
     processor.run()  # Generate the Markdown prompt for GPT
+    """
 
     # Step 11: Initialize the ChatGPT client, configure it, and get a response
     gpt_client = ChatGPTClient(username)
     gpt_client.set_configuration(max_tokens=2048, temperature=0.2)  # Set configuration for GPT
 
-    # Step 12: Create an instance of JsonFormatter for the given username and call the method to load, format, and save the JSON file
-    json_formatter = JsonFormatter(username)
-    json_formatter.load_and_format_json()
-
-    # Step 13: Get the response from GPT based on OCR text
+    # Step 12: Get the response from GPT based on OCR text
     response_text = gpt_client.get_response()
-
-    # Step 14: Check if the response was successfully generated and saved
+    
+    # Step 13: Check if the response was successfully generated
     if response_text:
         print(Fore.YELLOW + "\nResponse has been saved as _temp_receipt.json." + Style.RESET_ALL)
     else:
         print(Fore.RED + "No response received." + Style.RESET_ALL)
+    
+    # Step 14: Create an instance of JsonFormatter for the given username and call the method to load, format, and save the JSON file
+    json_formatter = JsonFormatter(username)
+    json_formatter.load_and_format_json()
 
     # Step 15: Add metadata to the JSON file
     json_updater = JsonUpdater(username, receipt_filename)
     json_updater.update_json_with_metadata()  # Update the JSON with metadata
 
+    # remove Reciept without contents    
+    remover = JsonReceiptRemover(username)
+    remover.remove_receipt()
+
+
     # Step 16: Send the receipt (e.g., email, API, etc.)
     sender = ReceiptSender(username)
     sender.send_receipt()  # Send the receipt
 
+    """
     # Step 17: If all tasks were successful, delete the temporary files for the user
     temp_deleter = TempFileDeleter(username)
     temp_deleter.run()  # Delete the temporary files and user-specific folder
-
+    """
 
 # Example usage
 if __name__ == "__main__":
